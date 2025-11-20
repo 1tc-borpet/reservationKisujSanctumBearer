f8ce83bd7b2a3d969f9db428275cb5474afd887e
1tc-borpet <borosp154@gmail.com>
2025-11-20 11:28:03 +0100

    teszteles

---
+++ tests/Feature/AuthControllerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+use App\Models\User;
+
+class AuthControllerTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    /*public function test_example(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }*/
+
+    use RefreshDatabase;
+    #[Test]
+    public function user_can_register()
+    {
+        //Arrange
+        $payload =[
+            'name'=>'boros3',
+            'email'=> 'boros3@moriczref.hu',
+            'password'=>'Jelszo_2025',
+            'password_confirmation'=>'Jelszo_2025',
+        ];
+        //Act
+        $response = $this->postJson('/api/register', $payload);
+        
+        //Assert
+        $response->assertStatus(201)->assertJsonStructure(['message','user']);
+        $this->assertDatabaseHas('users',['email'=>'boros3@moriczref.hu']);
+    }
+    #[Test]
+    public function user_can_login_and_receive_token(){
+        $user = User::factory()->create([
+            'email'=>'teszt@example.com',
+            'password'=>bcrypt('password'),
+        ]);
+        $credentials = [
+            'email'=>'teszt@example.com',
+            'password'=>bcrypt('password'),
+        ];
+        //Act
+        $response = $this->postJson('/api/login', $credentials);
+        //Assert
+        $response->assertStatus(200)->assertJsonStructure(['access_token','token_type']);
+    }
+    #[Test]
+    public function user_can_logout(){
+
+    }
+}
